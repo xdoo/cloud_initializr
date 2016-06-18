0016414be7be88417b8d705db45a32bc20379ec7
xdoo <claus.straube@gmail.com>
2016-06-18 07:40:49 +0200

    Added first rythm templates

--- service-creator/src/main/java/com/catify/initializr/services/ProxyGeneratorService.java
+++ service-creator/src/main/java/com/catify/initializr/services/ProxyGeneratorService.java
@@ -1,9 +1,11 @@
 package com.catify.initializr.services;
 
 import com.catify.initializr.domain.Domain;
+import java.io.File;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 import java.util.Map;
+import org.rythmengine.Rythm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +18,23 @@ public class ProxyGeneratorService {
 
     private final MavenStructureService mvn;
     private final DockerGeneratorService docker;
+    
+    private final File pomTemplate;
+    private final File propertiesTemplate;
+    private final File applicationTemplate;
 
     @Autowired
     public ProxyGeneratorService(MavenStructureService mvn, DockerGeneratorService docker) {
         this.mvn = mvn;
         this.docker = docker;
+        
+        // import templates
+        ClassLoader classLoader = getClass().getClassLoader();
+	this.pomTemplate = new File(classLoader.getResource("templates/proxy/pom.tmpl").getFile());
+        this.propertiesTemplate = new File(classLoader.getResource("templates/proxy/properties.tmpl").getFile());
+        this.applicationTemplate = new File(classLoader.getResource("templates/proxy/application.tmpl").getFile());
     }
-
+    
     public void createProxy(FileSystem fs, Domain domain) {
         // create maven project
         Map<String, Path> paths = this.mvn.createEmptyMavenProject(fs, domain.getPath(), "proxy");
@@ -38,7 +50,7 @@ public class ProxyGeneratorService {
 
     public void createPom(Map<String, Path> paths, String groupId) {
         Path path = paths.get(MavenStructureService.BASE).resolve("pom.xml");
-        String pom = String.format(this.pomTpl, groupId);
+        String pom = Rythm.render(this.pomTemplate, groupId);
         Util.writeToFile(pom, path);
     }
     
--- service-creator/src/test/java/com/catify/initializr/services/RythmTest.java
+++ service-creator/src/test/java/com/catify/initializr/services/RythmTest.java
@@ -23,4 +23,18 @@ public class RythmTest {
         String r02 = Rythm.render(hello, "Peter");
         Assert.assertEquals("Hello my friend Peter!", r02);
     }
+    
+    @Test
+    public void testMany() {
+        
+        ClassLoader classLoader = getClass().getClassLoader();
+	File hello = new File(classLoader.getResource("templates/hello.tmpl").getFile());
+        
+        System.out.println("started...");
+        
+        for (int i = 0; i < 1500; i++) {
+            String r01 = Rythm.render(hello, String.format("Paul_%s", i));
+            Assert.assertEquals(String.format("Hello my friend Paul_%s!", i), r01);
+        }
+    }
 }
